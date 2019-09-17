948345a68babe6fbffd11ff3b5fdceb4ab8d4a6f
Graham <webmaster@aisandbox.dev>
2019-09-17 22:40:48 +0100

    Javadoc updates

--- src/main/java/dev/aisandbox/client/AISandboxClient.java
+++ src/main/java/dev/aisandbox/client/AISandboxClient.java
@@ -14,6 +14,11 @@ import org.springframework.context.annotation.Configuration;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
+/**
+ * Launcher for the AI Sandbox Client.
+ * <p>
+ * This initialises Spring Boot, sets the FXML loader to use Spring then starts the JavaFX application.
+ */
 @SpringBootApplication
 @Configuration
 @ComponentScan
@@ -24,10 +29,22 @@ public class AISandboxClient extends Application {
     private ConfigurableApplicationContext context;
     private Parent rootNode;
 
+    /**
+     * The standard main method, that links to the JavaFX init method.
+     *
+     * @param args an array of {@link java.lang.String} objects.
+     */
     public static void main(String[] args) {
         Application.launch(args);
     }
 
+    /**
+     * The JavaFX initialisation method.
+     * <p>
+     * This is called during the start process, and includes the setup of the Spring context.
+     *
+     * @throws Exception
+     */
     @Override
     public void init() throws Exception {
         LOG.info("Initialising application");
@@ -39,6 +56,15 @@ public class AISandboxClient extends Application {
         rootNode = loader.load();
     }
 
+    /**
+     * JavaFX start method
+     * <p>
+     * Called after the init() method.
+     * <p>
+     * Opens the main window and centers on screen.
+     * @param primaryStage the main (empty) window
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         LOG.info("Starting application");
@@ -47,6 +73,10 @@ public class AISandboxClient extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Used by JavaFX when closing the application.
+     * @throws Exception
+     */
     @Override
     public void stop() throws Exception {
         LOG.info("Stopping application");
