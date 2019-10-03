41d7c78248d10753b2bd41d96717c390e57cc79b
Graham <webmaster@aisandbox.dev>
2019-10-03 22:48:30 +0100

    unit tests and javadoc improvements

--- src/main/java/dev/aisandbox/client/output/PNGOutputWriter.java
+++ src/main/java/dev/aisandbox/client/output/PNGOutputWriter.java
@@ -10,10 +10,9 @@ import java.util.Locale;
 import java.util.UUID;
 
 /**
- * <p>PNGOutputWriter class.</p>
- *
- * @author gde
- * @version $Id: $Id
+ * A class to write each video frame as a PNG within a subdirectory.
+ * <p>
+ * A new directory will be created and each frame will be written as a file nnnnnnnn.PNG
  */
 @Component
 public class PNGOutputWriter implements FrameOutput {
@@ -22,21 +21,38 @@ public class PNGOutputWriter implements FrameOutput {
     private File source = null;
 
     /**
-     * {@inheritDoc}
+     * Return the descriptive name of this output writer.
+     * <p>
+     * This is used when Spring initialises all available implementations of {@link FrameOutput} and displays them on the UI
+     * @param l a {@link java.util.Locale} object.
+     * @return the descriptive name "Write to files (PNG)"
      */
     @Override
     public String getName(Locale l) {
         return "Write to files (PNG)";
     }
 
-    /** {@inheritDoc} */
+    /**
+     * Create a new directory off the given root.
+     *
+     * @param baseDir The root directory - multiple runs will create multiple entries in this directory.
+     * @throws IOException thrown if the directory can't be created, or if open is called twice without closing.
+     */
     @Override
     public void open(File baseDir) throws IOException {
+        // check if we're already open
+        if (source != null) {
+            throw new IOException("Trying to open a stream which is already open");
+        }
         source = new File(baseDir, UUID.randomUUID().toString());
         source.mkdirs();
     }
 
-    /** {@inheritDoc} */
+    /**
+     * Add a single frame as a PNG image.
+     * @param frame a {@link java.awt.image.BufferedImage} object.
+     * @throws IOException thrown if the image can't be written
+     */
     @Override
     public void addFrame(BufferedImage frame) throws IOException {
         if (source == null) {
@@ -47,10 +63,14 @@ public class PNGOutputWriter implements FrameOutput {
         }
     }
 
-    /** {@inheritDoc} */
+    /**
+     * Finish writing files and reset for next open command
+     * @throws IOException
+     */
     @Override
     public void close() throws IOException {
         source = null;
+        count=0;
     }
 
 }
---
+++ src/test/java/dev/aisandbox/client/output/PNGOutputWriterTest.java
@@ -0,0 +1,65 @@
+package dev.aisandbox.client.output;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
+
+public class PNGOutputWriterTest {
+
+    private static final Logger LOG = Logger.getLogger(PNGOutputWriterTest.class.getName());
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void openTest() throws IOException {
+        // this should open a directory within the temp folder
+        LOG.info("Opening dir in " + folder.getRoot().getAbsolutePath());
+        PNGOutputWriter png = new PNGOutputWriter();
+        png.open(folder.getRoot());
+        // there should be a new directory
+        assertEquals("One directory created", folder.getRoot().listFiles().length, 1);
+    }
+
+    @Test
+    public void WriteTest() throws IOException {
+        // open a new session then write three frames
+        PNGOutputWriter png = new PNGOutputWriter();
+        png.open(folder.getRoot());
+        png.addFrame(OutputTools.getBlackScreen());
+        png.addFrame(OutputTools.getWhiteScreen());
+        png.addFrame(OutputTools.getColouredScreen(Color.RED));
+        png.close();
+        assertEquals("One directory created", folder.getRoot().listFiles().length, 1);
+        assertEquals("Three files in subdirectory", folder.getRoot().listFiles()[0].listFiles().length, 3);
+    }
+
+    @Test(expected = IOException.class)
+    public void doubleOpenTest() throws IOException {
+        // open a new session then try and open it without closing
+        PNGOutputWriter png = new PNGOutputWriter();
+        png.open(folder.getRoot());
+        png.addFrame(OutputTools.getBlackScreen());
+        png.open(folder.getRoot()); // THIS SHOULD THROW AN EXCEPTION
+        png.close();
+    }
+
+    @Test
+    public void openCloseTest() throws IOException {
+        // open a new session then try and open it without closing
+        PNGOutputWriter png = new PNGOutputWriter();
+        png.open(folder.getRoot());
+        png.addFrame(OutputTools.getBlackScreen());
+        png.close();
+        png.open(folder.getRoot()); // No exception
+        png.addFrame(OutputTools.getBlackScreen());
+        png.close();
+    }
+
+}
