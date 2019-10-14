a5d844c413ff0a1f58a2993de7899ab3c6b4d60c
Graham <webmaster@aisandbox.dev>
2019-10-14 14:32:31 +0100

    add POJO testing

---
+++ src/test/java/dev/aisandbox/client/scenarios/maze/api/POJOTests.java
@@ -0,0 +1,35 @@
+package dev.aisandbox.client.scenarios.maze.api;
+
+import org.junit.Test;
+import pl.pojo.tester.api.assertion.Method;
+
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+
+public class POJOTests {
+
+    @Test
+    public void PositionTest() {
+        assertPojoMethodsFor(Position.class).testing(Method.GETTER, Method.SETTER).areWellImplemented();
+    }
+
+    @Test
+    public void ConfigTest() {
+        assertPojoMethodsFor(Config.class).testing(Method.GETTER, Method.SETTER).areWellImplemented();
+    }
+
+    @Test
+    public void HistoryTest() {
+        assertPojoMethodsFor(History.class).testing(Method.GETTER, Method.SETTER).areWellImplemented();
+    }
+
+    @Test
+    public void MazeRequest() {
+        assertPojoMethodsFor(MazeRequest.class).testing(Method.GETTER, Method.SETTER).areWellImplemented();
+    }
+
+    @Test
+    public void MazeResponse() {
+        assertPojoMethodsFor(MazeResponse.class).testing(Method.GETTER, Method.SETTER).areWellImplemented();
+    }
+
+}
