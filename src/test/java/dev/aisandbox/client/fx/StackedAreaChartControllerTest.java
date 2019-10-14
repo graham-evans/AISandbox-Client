666e8f975b3eaf12046b8e302efe8ebe52e9935b
Graham <webmaster@aisandbox.dev>
2019-10-14 19:35:14 +0100

    Include headless code

--- src/test/java/dev/aisandbox/client/fx/StackedAreaChartControllerTest.java
+++ src/test/java/dev/aisandbox/client/fx/StackedAreaChartControllerTest.java
@@ -3,6 +3,7 @@ package dev.aisandbox.client.fx;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedAreaChart;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -13,9 +14,18 @@ import static org.junit.Assert.assertEquals;
 
 public class StackedAreaChartControllerTest extends ApplicationTest {
 
+    @BeforeClass
+    public static void setupHeadlessMode() {
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("java.awt.headless", "true");
+    }
+
     @Test
     public void SingleTest() {
-        StackedAreaChart areaChart = new StackedAreaChart(new CategoryAxis(), new NumberAxis());
+        StackedAreaChart areaChart = new StackedAreaChart(new NumberAxis(), new NumberAxis());
         StackedAreaChartController controller = new StackedAreaChartController(areaChart);
         Map<String, Double> reading = new TreeMap<>();
         reading.put("Initialise", 1.0);
@@ -26,7 +36,7 @@ public class StackedAreaChartControllerTest extends ApplicationTest {
 
     @Test
     public void resetTest() {
-        StackedAreaChart areaChart = new StackedAreaChart(new CategoryAxis(), new NumberAxis());
+        StackedAreaChart areaChart = new StackedAreaChart(new NumberAxis(), new NumberAxis());
         StackedAreaChartController controller = new StackedAreaChartController(areaChart);
         Map<String, Double> reading = new TreeMap<>();
         reading.put("Initialise", 1.0);
