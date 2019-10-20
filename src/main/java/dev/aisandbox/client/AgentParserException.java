f68756ec733a92bb0f7c286689de594fe7aec523
Graham <webmaster@aisandbox.dev>
2019-10-20 21:21:34 +0100

    added extra exceptions for agent

---
+++ src/main/java/dev/aisandbox/client/AgentParserException.java
@@ -0,0 +1,22 @@
+package dev.aisandbox.client;
+
+import lombok.Getter;
+
+public class AgentParserException extends AgentException {
+
+    @Getter
+    private final int responseCode;
+    @Getter
+    private final String response;
+
+    public AgentParserException(String message) {
+        super(message);
+        response = null;
+        responseCode=-1;
+    }
+    public AgentParserException(String message,int code,String content) {
+        super(message);
+        responseCode = code;
+        response=content;
+    }
+}
