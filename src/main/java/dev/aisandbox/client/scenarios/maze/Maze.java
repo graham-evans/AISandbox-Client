33e1f6eea7478a5f3e5cef20d74b5024847ba1e6
Graham <webmaster@aisandbox.dev>
2019-10-21 23:22:55 +0100

    Update Maze.java
    remove old imports

--- src/main/java/dev/aisandbox/client/scenarios/maze/Maze.java
+++ src/main/java/dev/aisandbox/client/scenarios/maze/Maze.java
@@ -1,16 +1,8 @@
 package dev.aisandbox.client.scenarios.maze;
 
 import dev.aisandbox.client.scenarios.maze.api.Config;
-import dev.aisandbox.client.sprite.SpriteLoader;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
