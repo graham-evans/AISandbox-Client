fa6308ef0bbb98010e882dd2b6365e0d47720ecc
Graham <webmaster@aisandbox.dev>
2019-11-18 16:46:19 +0000

    change descrption to textflow, add links for scenario and swagger (nont functioning)

--- src/main/java/dev/aisandbox/client/fx/GameChoiceController.java
+++ src/main/java/dev/aisandbox/client/fx/GameChoiceController.java
@@ -1,5 +1,8 @@
 package dev.aisandbox.client.fx;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -9,10 +12,14 @@ import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import dev.aisandbox.client.RuntimeModel;
 import dev.aisandbox.client.scenarios.Scenario;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -57,20 +64,30 @@ public class GameChoiceController {
     private ImageView gameImageField;
 
     @FXML
-    private Label gameDescriptionField;
+    private TextFlow gameDescription;
+
+    @FXML
+    private Hyperlink scenarioLink;
+
+    @FXML
+    private Hyperlink swaggerLink;
 
     @FXML
     private Button nextButton;
 
     @FXML
     void initialize() {
+
         assert gameList != null : "fx:id=\"gameList\" was not injected: check your FXML file 'GameChoice.fxml'.";
         assert titleText != null : "fx:id=\"titleText\" was not injected: check your FXML file 'GameChoice.fxml'.";
-        assert gameNameField != null : "fx:id=\"GameNameField\" was not injected: check your FXML file 'GameChoice.fxml'.";
-        assert gameIntroField != null : "fx:id=\"GameIntroField\" was not injected: check your FXML file 'GameChoice.fxml'.";
-        assert gameImageField != null : "fx:id=\"GameImageField\" was not injected: check your FXML file 'GameChoice.fxml'.";
-        assert gameDescriptionField != null : "fx:id=\"GameDescriptionField\" was not injected: check your FXML file 'GameChoice.fxml'.";
-        assert nextButton != null : "fx:id=\"NextButton\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert gameNameField != null : "fx:id=\"gameNameField\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert gameIntroField != null : "fx:id=\"gameIntroField\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert gameImageField != null : "fx:id=\"gameImageField\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert gameDescription != null : "fx:id=\"gameDescription\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert scenarioLink != null : "fx:id=\"scenarioLink\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert swaggerLink != null : "fx:id=\"swaggerLink\" was not injected: check your FXML file 'GameChoice.fxml'.";
+        assert nextButton != null : "fx:id=\"nextButton\" was not injected: check your FXML file 'GameChoice.fxml'.";
+
         LOG.info("Initialising Game Choise controller");
         if (scenarioList == null) {
             LOG.severe("scenarioList is null");
@@ -84,7 +101,8 @@ public class GameChoiceController {
             gameList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
                         gameNameField.setText(newValue.getName());
                         gameIntroField.setText(newValue.getOverview());
-                        gameDescriptionField.setText(newValue.getDescription());
+                        gameDescription.getChildren().clear();
+                        gameDescription.getChildren().add(convertStringToText(newValue.getDescription()));
                         if (newValue.getImageReference() != null) {
                             gameImageField.setImage(new Image(newValue.getImageReference()));
                         } else {
@@ -103,6 +121,30 @@ public class GameChoiceController {
         }
     }
 
+    private Text convertStringToText(String s) {
+        Text t = new Text(s);
+        return t;
+    }
+
+    @FXML
+    void launchScenarioLink(ActionEvent event) {
+        launchBrowser("https://www.aisandbox.dev/");
+    }
+
+    @FXML
+    void launchSwaggerLink(ActionEvent event) {
+        launchBrowser("https://www.aisandbox.dev/");
+    }
+
+    private void launchBrowser(String link) {
+        try {
+            Desktop.getDesktop().browse(new URL(link).toURI());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE,"IO Error spawning browser",e);
+        } catch (URISyntaxException e) {
+            LOG.log(Level.SEVERE,"URL Error spawning browser",e);
+        }
+    }
 
     @FXML
     void nextAction(ActionEvent event) {
