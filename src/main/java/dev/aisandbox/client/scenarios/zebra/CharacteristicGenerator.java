ce1d2a5344f32142430d92ec05a383d38933ac94
Graham <webmaster@aisandbox.dev>
2019-11-21 14:54:06 +0000

    add simple clues

--- src/main/java/dev/aisandbox/client/scenarios/zebra/CharacteristicGenerator.java
+++ src/main/java/dev/aisandbox/client/scenarios/zebra/CharacteristicGenerator.java
@@ -1,5 +1,6 @@
 package dev.aisandbox.client.scenarios.zebra;
 
+import java.io.Writer;
 import java.util.Collections;
 import java.util.Random;
 import java.util.logging.Level;
@@ -7,6 +8,13 @@ import java.util.logging.Logger;
 
 import com.thoughtworks.xstream.XStream;
 
+import com.thoughtworks.xstream.converters.basic.*;
+import com.thoughtworks.xstream.converters.collections.CollectionConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import com.thoughtworks.xstream.security.NoTypePermission;
 import dev.aisandbox.client.scenarios.zebra.vo.Characteristic;
 import dev.aisandbox.client.scenarios.zebra.vo.CharacteristicObject;
 import dev.aisandbox.client.scenarios.zebra.vo.Template;
@@ -15,13 +23,51 @@ public class CharacteristicGenerator {
 
     private static final Logger LOG = Logger.getLogger(CharacteristicGenerator.class.getName());
 
+    /**
+     * Using XStream in modern JDKs is becoming problematic, as it uses private access methods (which now throw warnings)
+     * To fix this we use the solution detailed here https://github.com/x-stream/xstream/issues/101
+     * <p>
+     * A longer-term solution may be to move away from xstream.
+     * @return an initialised XStream serialiser/deserialiser which doesn't use restricted methods
+     */
+    private static XStream createXStream() {
+        XStream xstream = new XStream(new StaxDriver() {
+            @Override
+            public HierarchicalStreamWriter createWriter(Writer out) {
+                return new PrettyPrintWriter(out, "    ");
+            }
+        }) {
+            // only register the converters we need; other converters generate a private access warning in the console on Java9+...
+            @Override
+            protected void setupConverters() {
+                registerConverter(new NullConverter(), PRIORITY_VERY_HIGH);
+                registerConverter(new IntConverter(), PRIORITY_NORMAL);
+                registerConverter(new FloatConverter(), PRIORITY_NORMAL);
+                registerConverter(new DoubleConverter(), PRIORITY_NORMAL);
+                registerConverter(new LongConverter(), PRIORITY_NORMAL);
+                registerConverter(new ShortConverter(), PRIORITY_NORMAL);
+                registerConverter(new BooleanConverter(), PRIORITY_NORMAL);
+                registerConverter(new ByteConverter(), PRIORITY_NORMAL);
+                registerConverter(new StringConverter(), PRIORITY_NORMAL);
+                registerConverter(new DateConverter(), PRIORITY_NORMAL);
+                registerConverter(new CollectionConverter(getMapper()), PRIORITY_NORMAL);
+                registerConverter(new ReflectionConverter(getMapper(), getReflectionProvider()), PRIORITY_VERY_LOW);
+            }
+        };
+        xstream.autodetectAnnotations(true);
+
+        // setup proper security by limiting which classes can be loaded by XStream
+        xstream.addPermission(NoTypePermission.NONE);
+        xstream.processAnnotations(Template.class);
+        xstream.allowTypes(new Class[]{Template.class, Characteristic.class, CharacteristicObject.class});
+        return xstream;
+    }
+
+
     public static Template createTemplate(Random rand,int characteristics,int houses) {
         Template t = null;
         try {
-            XStream x = new XStream();
-            // configure
-            x.processAnnotations(Template.class);
-            x.allowTypes(new Class[]{Template.class, Characteristic.class, CharacteristicObject.class});
+            XStream x = createXStream();
             // read
             t = (Template) x.fromXML(CharacteristicGenerator.class.getResourceAsStream("template.xml"));
             // randomise the characteristics
