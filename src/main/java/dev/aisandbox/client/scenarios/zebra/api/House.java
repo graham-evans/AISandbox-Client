1a18e9575484a1551ff4715e5c0754e270427b5f
Graham <webmaster@aisandbox.dev>
2019-11-29 13:30:31 +0000

    Create House.java

---
+++ src/main/java/dev/aisandbox/client/scenarios/zebra/api/House.java
@@ -0,0 +1,18 @@
+package dev.aisandbox.client.scenarios.zebra.api;
+
+import lombok.Data;
+
+@Data
+public class House {
+    int houseNumber;
+    String doorColour;
+    String curtainColour;
+    String likesToEat;
+    String likesToDrink;
+    String nationality;
+    String holiday;
+    String sport;
+    String pet;
+    String flowers;
+    String drives;
+}
