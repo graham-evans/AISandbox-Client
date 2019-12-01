39b84d4531de83bdbab48443386f19647713deeb
Graham <webmaster@aisandbox.dev>
2019-12-01 19:47:33 +0000

    BUGFIX - XML parse error on response

--- src/main/java/dev/aisandbox/client/scenarios/maze/api/MazeResponse.java
+++ src/main/java/dev/aisandbox/client/scenarios/maze/api/MazeResponse.java
@@ -1,7 +1,10 @@
 package dev.aisandbox.client.scenarios.maze.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import dev.aisandbox.client.scenarios.ServerResponse;
 import lombok.Data;
+
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * <p>MazeResponse class.</p>
@@ -9,7 +12,8 @@ import lombok.Data;
  * @author gde
  * @version $Id: $Id
  */
-
+@JsonIgnoreProperties(ignoreUnknown = true)
+@XmlRootElement(name = "MazeResponse")
 @Data
 public class MazeResponse implements ServerResponse {
     String move;
--- src/main/java/dev/aisandbox/client/scenarios/mine/api/MineHunterResponse.java
+++ src/main/java/dev/aisandbox/client/scenarios/mine/api/MineHunterResponse.java
@@ -1,9 +1,14 @@
 package dev.aisandbox.client.scenarios.mine.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import dev.aisandbox.client.scenarios.ServerResponse;
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@XmlRootElement(name = "MineResponse")
 public class MineHunterResponse implements ServerResponse {
     Move[] moves;
 }
