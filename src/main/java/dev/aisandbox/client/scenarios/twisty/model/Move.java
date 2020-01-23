445cf43fc003a569a6ec03167d2e4c1fe3b28836
aisandbox <graham.evans@aisandbox.dev>
2020-01-23 10:15:39 +0000

    Basic model implemented

---
+++ src/main/java/dev/aisandbox/client/scenarios/twisty/model/Move.java
@@ -0,0 +1,12 @@
+package dev.aisandbox.client.scenarios.twisty.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Move {
+    private int cost;
+    private List<MoveChain> chains = new ArrayList<>();
+}
