9f94400567f8bd090363f298b0611346328e7bfa
Graham <graham-evans@users.noreply.github.com>
2020-05-11 17:44:35 +0100

    reworked tests for POST method

--- src/test/java/dev/aisandbox/client/agent/AgentLoggerTest.java
+++ src/test/java/dev/aisandbox/client/agent/AgentLoggerTest.java
@@ -1,10 +1,13 @@
 package dev.aisandbox.client.agent;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
+import dev.aisandbox.client.scenarios.TestRequest;
 import dev.aisandbox.client.scenarios.TestResponse;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -20,20 +23,24 @@ public class AgentLoggerTest {
   private static final String JSON_TEXT = "{\"number\":\"4\"}";
 
   @Test
-  public void testGetJSON() throws AgentException {
+  public void testLogger() throws AgentException {
     Agent a = new Agent();
-    a.setTarget("http://localhost/getJSON");
+    a.setTarget("http://localhost/postJSON");
     a.setEnableXML(false);
     a.setupAgent();
     // setup mock server
     MockRestServiceServer server = AgentMockTool.createMockServer(a);
-
     // setup expectations
     server
-        .expect(requestTo("http://localhost/getJSON"))
-        .andExpect(method(HttpMethod.GET))
-        .andRespond(withSuccess(JSON_TEXT, MediaType.APPLICATION_JSON));
-    TestResponse r = a.getRequest("", TestResponse.class);
+        .expect(requestTo("http://localhost/postJSON"))
+        .andExpect(method(HttpMethod.POST))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("name").value("Betty"))
+        .andRespond(withSuccess("{\"number\":\"4\"}", MediaType.APPLICATION_JSON));
+    // run request
+    TestRequest req = new TestRequest();
+    req.setName("Betty");
+    TestResponse r = a.postRequest(req, TestResponse.class);
     server.verify();
     assertEquals("Answer=4", 4, r.getNumber());
     // check the correct response was recieved
