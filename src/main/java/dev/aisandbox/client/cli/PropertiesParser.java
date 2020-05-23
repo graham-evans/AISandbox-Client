26e6ae41db47cd2a2308e771c38d4c4d108ec966
Graham <graham-evans@users.noreply.github.com>
2020-05-23 20:44:57 +0100

    temp disable scenario parameters

--- src/main/java/dev/aisandbox/client/cli/PropertiesParser.java
+++ src/main/java/dev/aisandbox/client/cli/PropertiesParser.java
@@ -3,11 +3,6 @@ package dev.aisandbox.client.cli;
 import dev.aisandbox.client.RuntimeModel;
 import dev.aisandbox.client.agent.Agent;
 import dev.aisandbox.client.output.OutputFormat;
-import dev.aisandbox.client.scenarios.maze.MazeScenario;
-import dev.aisandbox.client.scenarios.maze.MazeSize;
-import dev.aisandbox.client.scenarios.maze.MazeType;
-import dev.aisandbox.client.scenarios.mine.MineHunterScenario;
-import dev.aisandbox.client.scenarios.mine.SizeEnum;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -151,26 +146,26 @@ public class PropertiesParser {
    * @param props the properties to scan
    */
   public void readMineSettings(RuntimeModel model, Properties props) {
-    MineHunterScenario mine = appContext.getBean(MineHunterScenario.class);
-    model.setScenario(mine);
-    if (props.containsKey("salt")) {
-      mine.setScenarioSalt(Long.parseLong(props.getProperty("salt")));
-    }
-    if (props.containsKey("size")) {
-      switch (props.getProperty("size")) {
-        case "small":
-          mine.setMineHunterBoardSize(SizeEnum.SMALL);
-          break;
-        case "medium":
-          mine.setMineHunterBoardSize(SizeEnum.MEDIUM);
-          break;
-        case "large":
-          mine.setMineHunterBoardSize(SizeEnum.LARGE);
-          break;
-        default:
-          log.warn("unrecognised board size");
-      }
-    }
+    //    MineHunterScenario mine = appContext.getBean(MineHunterScenario.class);
+    //    model.setScenario(mine);
+    //    if (props.containsKey("salt")) {
+    //      mine.setScenarioSalt(Long.parseLong(props.getProperty("salt")));
+    //    }
+    //    if (props.containsKey("size")) {
+    //      switch (props.getProperty("size")) {
+    //        case "small":
+    //          mine.setMineHunterBoardSize(SizeEnum.SMALL);
+    //          break;
+    //        case "medium":
+    //          mine.setMineHunterBoardSize(SizeEnum.MEDIUM);
+    //          break;
+    //        case "large":
+    //          mine.setMineHunterBoardSize(SizeEnum.LARGE);
+    //          break;
+    //        default:
+    //          log.warn("unrecognised board size");
+    //      }
+    //    }
   }
 
   /**
@@ -180,43 +175,43 @@ public class PropertiesParser {
    * @param props the properties to scan
    */
   public void readMazeSettings(RuntimeModel model, Properties props) {
-    MazeScenario maze = appContext.getBean(MazeScenario.class);
-    model.setScenario(maze);
-    if (props.containsKey("salt")) {
-      maze.setScenarioSalt(Long.parseLong(props.getProperty("salt")));
-    }
-    if (props.containsKey("size")) {
-      switch (props.getProperty("size")) {
-        case "small":
-          maze.setMazeSize(MazeSize.SMALL);
-          break;
-        case "medium":
-          maze.setMazeSize(MazeSize.MEDIUM);
-          break;
-        case "large":
-          maze.setMazeSize(MazeSize.LARGE);
-          break;
-        default:
-          log.warn("unrecognised board size");
-      }
-    }
-    if (props.containsKey("type")) {
-      switch (props.getProperty("type")) {
-        case "binarytree":
-          maze.setMazeType(MazeType.BINARYTREE);
-          break;
-        case "sidewinder":
-          maze.setMazeType(MazeType.SIDEWINDER);
-          break;
-        case "recursivebacktracker":
-          maze.setMazeType(MazeType.RECURSIVEBACKTRACKER);
-          break;
-        case "braided":
-          maze.setMazeType(MazeType.BRAIDED);
-          break;
-        default:
-          log.warn("unrecognised maze type");
-      }
-    }
+    //    MazeScenario maze = appContext.getBean(MazeScenario.class);
+    //    model.setScenario(maze);
+    //    if (props.containsKey("salt")) {
+    //      maze.setScenarioSalt(Long.parseLong(props.getProperty("salt")));
+    //    }
+    //    if (props.containsKey("size")) {
+    //      switch (props.getProperty("size")) {
+    //        case "small":
+    //          maze.setMazeSize(MazeSize.SMALL);
+    //          break;
+    //        case "medium":
+    //          maze.setMazeSize(MazeSize.MEDIUM);
+    //          break;
+    //        case "large":
+    //          maze.setMazeSize(MazeSize.LARGE);
+    //          break;
+    //        default:
+    //          log.warn("unrecognised board size");
+    //      }
+    //    }
+    //    if (props.containsKey("type")) {
+    //      switch (props.getProperty("type")) {
+    //        case "binarytree":
+    //          maze.setMazeType(MazeType.BINARYTREE);
+    //          break;
+    //        case "sidewinder":
+    //          maze.setMazeType(MazeType.SIDEWINDER);
+    //          break;
+    //        case "recursivebacktracker":
+    //          maze.setMazeType(MazeType.RECURSIVEBACKTRACKER);
+    //          break;
+    //        case "braided":
+    //          maze.setMazeType(MazeType.BRAIDED);
+    //          break;
+    //        default:
+    //          log.warn("unrecognised maze type");
+    //      }
+    //    }
   }
 }
