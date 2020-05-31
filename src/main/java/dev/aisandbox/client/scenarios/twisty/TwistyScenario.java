17d1456d26f969e0804ffbbc65e9dd8801e19950
Graham <graham-evans@users.noreply.github.com>
2020-05-31 16:48:23 +0100

    all cubes available

--- src/main/java/dev/aisandbox/client/scenarios/twisty/TwistyScenario.java
+++ src/main/java/dev/aisandbox/client/scenarios/twisty/TwistyScenario.java
@@ -7,7 +7,15 @@ import dev.aisandbox.client.scenarios.Scenario;
 import dev.aisandbox.client.scenarios.ScenarioParameter;
 import dev.aisandbox.client.scenarios.ScenarioRuntime;
 import dev.aisandbox.client.scenarios.ScenarioType;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube10x10x10;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube2x2x2;
 import dev.aisandbox.client.scenarios.twisty.puzzles.Cube3x3x3;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube4x4x4;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube5x5x5;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube6x6x6;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube7x7x7;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube8x8x8;
+import dev.aisandbox.client.scenarios.twisty.puzzles.Cube9x9x9;
 import java.util.Random;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +33,17 @@ import org.springframework.stereotype.Component;
 public class TwistyScenario implements Scenario {
 
   private static final String[] PUZZLE_TYPES =
-      new String[] {"Cube 3x3x3 (OBTM)", "Cube 2x2x2 (OBTM)"};
+      new String[] {
+        "Cube 2x2x2 (OBTM)",
+        "Cube 3x3x3 (OBTM)",
+        "Cube 4x4x4 (OBTM)",
+        "Cube 5x5x5 (OBTM)",
+        "Cube 6x6x6 (OBTM)",
+        "Cube 7x7x7 (OBTM)",
+        "Cube 8x8x8 (OBTM)",
+        "Cube 9x9x9 (OBTM)",
+        "Cube 10x10x10 (OBTM)"
+      };
 
   private OptionParameter twistyType = new OptionParameter("twisty.type", PUZZLE_TYPES);
   private LongParameter scenarioSalt = new LongParameter("twisty.salt", 0, "Random Salt", null);
@@ -74,24 +92,6 @@ public class TwistyScenario implements Scenario {
     return 1;
   }
 
-  //  /** {@inheritDoc} */
-  //  @Override
-  //  public void startSimulation(
-  //      List<Agent> agentList, GameRunController ui, FrameOutput output, Long stepCount) {
-  //    // create random number generator
-  //    Random rand;
-  //    if (scenarioSalt == 0) {
-  //      rand = new Random();
-  //    } else {
-  //      rand = new Random(scenarioSalt);
-  //    }
-  //    log.info("Starting run thread");
-  //    thread =
-  //        new TwistyThread(
-  //            agentList.get(0), output, ui, rand, twistyType, stepCount, twistyStartSolved);
-  //    thread.start();
-  //  }
-
   /** {@inheritDoc} */
   @Override
   public String getScenarioURL() {
@@ -112,7 +112,38 @@ public class TwistyScenario implements Scenario {
   @Override
   public ScenarioRuntime getRuntime() {
     TwistyRuntime runtime = new TwistyRuntime();
-    runtime.setPuzzle(new Cube3x3x3());
+    switch (twistyType.getOptionIndex()) {
+      case 0:
+        runtime.setPuzzle(new Cube2x2x2());
+        break;
+      case 1:
+        runtime.setPuzzle(new Cube3x3x3());
+        break;
+      case 2:
+        runtime.setPuzzle(new Cube4x4x4());
+        break;
+      case 3:
+        runtime.setPuzzle(new Cube5x5x5());
+        break;
+      case 4:
+        runtime.setPuzzle(new Cube6x6x6());
+        break;
+      case 5:
+        runtime.setPuzzle(new Cube7x7x7());
+        break;
+      case 6:
+        runtime.setPuzzle(new Cube8x8x8());
+        break;
+      case 7:
+        runtime.setPuzzle(new Cube9x9x9());
+        break;
+      case 8:
+        runtime.setPuzzle(new Cube10x10x10());
+        break;
+      default:
+        log.error("Unknown puzzle type");
+        runtime.setPuzzle(new Cube3x3x3());
+    }
     if (scenarioSalt.getValue() != 0) {
       runtime.setRandom(new Random(scenarioSalt.getValue()));
     }
