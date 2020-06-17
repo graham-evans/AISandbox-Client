cf37ad5b1b4beac86d4189c294bfb50def44c6ca
Graham <graham-evans@users.noreply.github.com>
2020-06-17 20:01:09 +0100

    reinstate mine test

--- src/test/java/dev/aisandbox/client/scenarios/mine/MineRunTest.java
+++ src/test/java/dev/aisandbox/client/scenarios/mine/MineRunTest.java
@@ -1,25 +1,41 @@
 package dev.aisandbox.client.scenarios.mine;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import dev.aisandbox.client.ApplicationModel;
+import dev.aisandbox.client.SimulationRunThread;
 import dev.aisandbox.client.cli.PropertiesParser;
+import dev.aisandbox.client.fx.FakeGameRunController;
+import dev.aisandbox.client.scenarios.maze.MazeScenario;
+import dev.aisandbox.client.scenarios.maze.agent.MazeTestAgent;
+import dev.aisandbox.client.scenarios.mine.agent.MineTestAgent;
 import dev.aisandbox.launcher.AISandboxCLI;
 import java.io.File;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {AISandboxCLI.class})
 @SpringBootTest
+@Slf4j
 public class MineRunTest {
 
   @Autowired PropertiesParser parser;
 
   @Autowired ApplicationModel model;
+
+  @Autowired ApplicationContext context;
 
   @Before
   public void resetDir() {
@@ -42,31 +58,37 @@ public class MineRunTest {
 
   @Test(timeout = 100000) // shouldn't take more than 100 seconds
   public void runFullMineTest() throws Exception {
-    //    System.out.println("Running simulation manualy");
-    //    SpringApplicationBuilder builder = new SpringApplicationBuilder(AISandboxCLI.class);
-    //    builder.headless(true);
-    //    ConfigurableApplicationContext context = builder.run();
-    //    // generate the default model and update with CLI (and XML) options
-    //    parser.parseConfiguration(model, "src/test/resources/config/mine.properties");
-    //    // test config has been loaded
-    //    assertNotNull("Scenario doesn't exist", model.getScenario());
-    //    assertTrue("Incorrect Scenario", model.getScenario() instanceof MineHunterScenario);
-    //    // manualy attach test agent
-    //    model.getAgentList().clear();
-    //    model.getAgentList().add(new MineTestAgent());
-    //    assertTrue("Model not ready", model.getValid().get());
-    //    // manualy run the model
-    //    FrameOutput out = new PNGOutputWriter();
-    //    File dest = new File("target/test-run/mine1");
-    //    out.open(dest);
-    //    model
-    //        .getScenario()
-    //        .startSimulation(model.getAgentList(), new FakeGameRunController(), out, 10l);
-    //    model.getScenario().joinSimulation();
-    //    // we should now have a single directory inside maze1
-    //    assertEquals("Number of subdirectories in target", 1, dest.listFiles().length);
-    //    File dest2 = dest.listFiles()[0];
-    //    // we should have 10 images in this directory
-    //    assertTrue("Too few images", 10 <= dest2.listFiles().length);
+    System.out.println("Running simulation manually");
+    SpringApplicationBuilder builder = new SpringApplicationBuilder(AISandboxCLI.class);
+    builder.headless(true);
+    ConfigurableApplicationContext context = builder.run();
+    // generate the default model and update with CLI (and XML) options
+    parser.parseConfiguration(model, "src/test/resources/config/mine.properties");
+    // test config has been loaded
+    assertNotNull("Scenario doesn't exist", model.getScenario());
+    assertTrue("Incorrect Scenario", model.getScenario() instanceof MineHunterScenario);
+    // manually attach test agent
+    model.getAgentList().clear();
+    model.getAgentList().add(new MineTestAgent());
+    assertTrue("Model not ready", model.getValid().get());
+    // setup a fake UI
+    model.initialiseRuntime(new FakeGameRunController(context, model, null));
+    // run the model
+    SimulationRunThread thread;
+    if (model.getLimitRuntime().get()) {
+      thread = new SimulationRunThread(model, model.getMaxStepCount().get());
+    } else {
+      thread = new SimulationRunThread(model);
+    }
+    thread.start();
+    thread.join();
+    model.resetRuntime();
+    // we should now have a single directory inside maze1
+    File dest = new File("target/run/mine1");
+    assertTrue("Target directory exists", dest.isDirectory());
+    assertEquals("Number of subdirs", 1, dest.listFiles().length);
+    File job = dest.listFiles()[0];
+    // we should have 10 images in this directory
+    assertEquals("Number of images", 10, job.listFiles().length);
   }
 }
