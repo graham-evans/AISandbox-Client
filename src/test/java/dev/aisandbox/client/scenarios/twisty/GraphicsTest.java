1ee8f4352f2b3b6c3f28cad608d8ce0fa8ae9e61
Graham <graham-evans@users.noreply.github.com>
2021-01-02 00:51:14 +0000

    update package

--- src/test/java/dev/aisandbox/client/scenarios/twisty/GraphicsTest.java
+++ src/test/java/dev/aisandbox/client/scenarios/twisty/GraphicsTest.java
@@ -1,48 +1,26 @@
-package dev.aisandbox.client.scenarios.twisty.puzzles;
+package dev.aisandbox.client.scenarios.twisty;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import dev.aisandbox.client.scenarios.twisty.NotExistentMoveException;
-import dev.aisandbox.client.scenarios.twisty.TwistyPuzzle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javafx.scene.text.Font;
 import javax.imageio.ImageIO;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GraphicsTest {
 
   static List<TwistyPuzzle> puzzleList = new ArrayList();
 
-  @BeforeClass
-  public static void setupPuzzleList() {
-    // load font
-    Font.loadFont(GraphicsTest.class.getResourceAsStream("/fonts/Hack-Regular.ttf"), 12.0);
-    // load puzzles
-    puzzleList.add(new Cube2x2x2());
-    puzzleList.add(new Cube3x3x3());
-    puzzleList.add(new Cube4x4x4());
-    puzzleList.add(new Cube5x5x5());
-    puzzleList.add(new Cube6x6x6());
-    puzzleList.add(new Cube7x7x7());
-    puzzleList.add(new Cube8x8x8());
-    puzzleList.add(new Cube9x9x9());
-    puzzleList.add(new Cube10x10x10());
-    puzzleList.add(
-        new TPPuzzle("/dev/aisandbox/client/scenarios/twisty/tppuzzles/Pyramid3.tp"));
-  }
-
   @Test
   public void createImageTest() throws IOException {
-    assertNotEquals("No Puzzles to test", 0, puzzleList.size());
-    for (TwistyPuzzle puzzle : puzzleList) {
+    for (PuzzleType pt : PuzzleType.values()) {
+      TPPuzzle puzzle = new TPPuzzle(pt.getResource(), pt.getID());
       puzzle.resetPuzzle();
       File out = new File("target/test-images/twisty/" + puzzle.getPuzzleName() + "/puzzle.png");
       out.getParentFile().mkdirs();
@@ -54,8 +32,8 @@ public class GraphicsTest {
 
   @Test
   public void createMoveImageTest() throws IOException, NotExistentMoveException {
-    assertNotEquals("No Puzzles to test", 0, puzzleList.size());
-    for (TwistyPuzzle puzzle : puzzleList) {
+    for (PuzzleType pt : PuzzleType.values()) {
+      TPPuzzle puzzle = new TPPuzzle(pt.getResource(), pt.getID());
       for (int i = 0; i < puzzle.getMoveList().size(); i++) {
         String move = puzzle.getMoveList().get(i);
         // create image showing the result of the move from the starting positions
@@ -100,7 +78,8 @@ public class GraphicsTest {
 
   @Test
   public void createMoveSpriteSheet() throws IOException, NotExistentMoveException {
-    for (TwistyPuzzle puzzle : puzzleList) {
+    for (PuzzleType pt : PuzzleType.values()) {
+      TPPuzzle puzzle = new TPPuzzle(pt.getResource(), pt.getID());
       puzzle.resetPuzzle();
       File out = new File("target/test-images/twisty/" + puzzle.getPuzzleName() + "/sprites.png");
       out.getParentFile().mkdirs();
